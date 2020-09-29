7241b4760240f8ef323b402353babd14b80caf0b
Kieran Ryan <kryan@ub.edu.bz>
2020-09-29 15:46:35 -0600

    First Commit

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -12,10 +12,10 @@ export class QuizService {
 
   getAll() {
     return [
-      { id: 'data/javascript.json', name: 'JavaScript' },
-      { id: 'data/aspnet.json', name: 'Asp.Net' },
-      { id: 'data/csharp.json', name: 'C Sharp' },
-      { id: 'data/designPatterns.json', name: 'Design Patterns' }
+      { id: 'data/javascript.json', name: 'PSE Math Quiz 1' },
+      { id: 'data/aspnet.json', name: 'PSE Math Quiz 2' }
+      // { id: 'data/csharp.json', name: 'PSE Math Quiz 3' },
+      // { id: 'data/designPatterns.json', name: 'PSE Math Quiz 4' }
     ];
   }
 
